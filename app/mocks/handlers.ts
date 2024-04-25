062e958ff0bf35159a44c047266756c95572225f
isimmons <isimmons33@gmail.com>
2024-04-25 16:15:32 -0400

    first intercept and mock

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -1,1 +1,12 @@
-export const handlers = [];
+import { http } from "msw";
+import { movies } from "./data";
+
+export const handlers = [
+  http.get("https://api.example.com/movies/featured", () => {
+    return new Response(JSON.stringify(movies), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }),
+];
