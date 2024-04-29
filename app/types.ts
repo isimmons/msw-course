03809c68e7ad904725017f70e727190b81d5aabe
isimmons <isimmons33@gmail.com>
2024-04-29 00:05:19 -0400

    formatted

--- app/types.ts
+++ app/types.ts
@@ -1,63 +1,63 @@
 export interface User {
-  id: string
-  email: string
-  firstName: string
-  lastName: string
-  avatarUrl: string
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  avatarUrl: string;
 }
 
 export interface Movie {
-  id: string
-  slug: string
-  title: string
-  category: string
-  releasedAt: Date
-  description: string
-  imageUrl: string
+  id: string;
+  slug: string;
+  title: string;
+  category: string;
+  releasedAt: Date;
+  description: string;
+  imageUrl: string;
 }
 
 export interface FeaturedMovie extends Movie {}
 
 export interface MovieReview {
-  id: string
-  text: string
-  rating: number
-  author: User
+  id: string;
+  text: string;
+  rating: number;
+  author: User;
 }
 
 export interface ListReviewsQuery {
-  reviews: Array<MovieReview>
+  reviews: Array<MovieReview>;
 }
 
 export interface ListReviewQueryVariables {
-  movieId: string
+  movieId: string;
 }
 
 export interface AddReviewMutation {
   addReview: {
-    id: string
-    text: string
-  }
+    id: string;
+    text: string;
+  };
 }
 
 export interface AddReviewMutationVariables {
-  author: Pick<User, 'id' | 'firstName' | 'avatarUrl'>
+  author: Pick<User, "id" | "firstName" | "avatarUrl">;
   reviewInput: {
-    movieId: string
-    text: string
-    rating: number
-  }
+    movieId: string;
+    text: string;
+    rating: number;
+  };
 }
 
 export interface Series {
-  id: string
-  title: string
-  category: string
-  episodes: Array<Episode>
+  id: string;
+  title: string;
+  category: string;
+  episodes: Array<Episode>;
 }
 
 export interface Episode {
-  id: string
-  title: string
-  durationMinutes: number
+  id: string;
+  title: string;
+  durationMinutes: number;
 }
